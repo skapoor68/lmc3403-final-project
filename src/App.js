63130feb4e9c8019cd9665553386a4f62dd440a1
skapoor68 <skapoor68@gatech.edu>
2024-11-20 16:22:28 -0500

    gh pages deployment

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import HomePage from './components/HomePage';
 import ResumeScreening from './components/ResumeScreening';
 
 function App() {
+  const basename = process.env.NODE_ENV === 'production' 
+    ? '/lmc3403-final-project'
+    : '';
+
   return (
-    <Router>
+    <Router basename={basename}>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
